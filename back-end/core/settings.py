a8c1c71e5f4c36bac42fccd4ece6884600c1add7
eudivanmelo <eudivan44@gmail.com>
2025-05-01 11:24:47 -0300

    feat: configure CORS and add Task app to settings

--- back-end/core/settings.py
+++ back-end/core/settings.py
@@ -16,6 +16,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOW_ALL_ORIGINS = True # Permitir todas as origens para desenvolvimento
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    # "https://seu-frontend.com",
+]
 
 # Application definition
 
@@ -27,11 +32,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'corsheaders',
+
     'rest_framework',
     'task',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
